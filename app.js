dcc3e5b7df1d510f2ace11c7ae7d8a0c37e747b2
kischin <cwrona2@uwo.ca>
2023-03-02 20:54:37 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const year = document.getElementById("year");
+
+year.textContent = new Date().getFullYear();
+
+let menu = document.getElementById("nav-icon4");
+let menuList = document.getElementById("menu");
+
+
+
+
+menu.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  menuList.classList.toggle('open');
+  document.body.classList.toggle('open');
+  // if(menuList.classList.contains('open')) {
+  //   menuList.style.opacity = 1;
+  // }
+  // else menuList.style.opacity = 0;
+})
+
+let navlinks = document.querySelectorAll('li');
+
+
+navlinks.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    console.log('yay');
+    document.body.classList.toggle('open');
+    menu.classList.toggle('open');
+    menuList.classList.toggle('open');
+  })
+});
+// navlink.addEventListener('click', () => {
+//   menu.classList.toggle('open');
+//   menuList.classList.toggle('open');
+// })
+
